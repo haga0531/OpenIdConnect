b251a17cb060573eda96d6d17d46957f8a61a25b
Hiroki Haga <hiroki.haga@sansan.com>
2024-11-04 21:30:45 +0900

    Add introspect endpoint

---
+++ Idp/Controllers/IntrospectController.cs
@@ -0,0 +1,22 @@
+﻿using Idp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Idp.Controllers;
+
+[Route("openid-connect")]
+public class IntrospectController(Context context) : Controller
+{
+    [HttpPost("introspect")]
+    public IActionResult Introspect([FromForm] IntrospectRequest request)
+    {
+        var foundAccessToken = context.AccessTokens.FirstOrDefault(x => x.Token == request.Token);
+
+        if (foundAccessToken == null || foundAccessToken.ExpiresAt <
+            Convert.ToInt32((DateTimeOffset.UtcNow - DateTimeOffset.UnixEpoch).TotalSeconds))
+        {
+            return Unauthorized(new { active = false });
+        }
+
+        return Ok(new { active = true });
+    }
+}
---
+++ Idp/Models/IntrospectRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Idp.Models;
+
+public class IntrospectRequest
+{
+    [FromForm(Name = "token")]
+    public string Token { get; set; } = null!;
+}
