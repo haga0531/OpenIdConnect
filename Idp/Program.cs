45a264b8837dd2aa65a37b4c725c5db8aeefd18d
Hiroki Haga <hiroki.haga@sansan.com>
2024-11-04 17:11:47 +0900

    Add service for using mvc

--- Idp/Program.cs
+++ Idp/Program.cs
@@ -3,6 +3,8 @@ using Idp.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+
 var context = new Context
 {
     Users = new List<User>
